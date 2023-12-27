9d615b8d3bbe3192a05400a4e35d9da3b66b1fa3
Scott <scottld@stud.ntnu.no>
2023-12-27 22:52:21 +0100

    Added a ball class w methods

--- pong.py
+++ pong.py
@@ -14,28 +14,28 @@ class Paddle:
     def __init__(self,xPos, yPos, rightPaddle:bool):
         self.xPos = xPos
         self.yPos = yPos
-        self.fart = 0.2
+        self.speed = 0.2
         self.rightPaddle = rightPaddle
         self.width = 10
         self.length = 70
         self.points = 0
     
-    def movePaddle(self,tasterTrykket):
+    def movePaddle(self,keysPressed):
         if self.yPos < 0:
             self.yPos = 1
         elif self.yPos+self.length > 600:
             self.yPos = 599-self.length
 
         if self.rightPaddle:
-            if tasterTrykket[K_UP]:
-                self.yPos-=self.fart
-            elif tasterTrykket[K_DOWN]:
-                self.yPos+=self.fart
+            if keysPressed[K_UP]:
+                self.yPos-=self.speed
+            elif keysPressed[K_DOWN]:
+                self.yPos+=self.speed
         else:
-            if tasterTrykket[K_w]:
-                self.yPos-=self.fart
-            elif tasterTrykket[K_s]:
-                self.yPos+=self.fart
+            if keysPressed[K_w]:
+                self.yPos-=self.speed
+            elif keysPressed[K_s]:
+                self.yPos+=self.speed
         
         self.drawPaddle()
     
@@ -46,15 +46,39 @@ class Paddle:
         return pg.Rect(self.xPos,self.yPos,self.width,self.length)
 
 
+class Ball:
+    def __init__(self, xPos,yPos,rad):
+        self.xPos = xPos
+        self.yPos = yPos
+        self.rad = rad
+        self.xSpeed = 0
+        self.ySpeed = 0.1
+    
+    def drawBall(self):
+        pg.draw.rect(vindu, (0,0,0), (self.xPos,self.yPos, self.rad,self.rad))
+    
+    def getRect(self):
+        return pg.Rect(self.xPos,self.yPos,self.rad,self.rad)
+    
+    def moveBall(self):
+        if self.yPos <=0 or self.yPos >= 600:
+            self.ySpeed = -self.ySpeed
+        self.xPos += self.xSpeed
+        self.yPos += self.ySpeed
+        self.drawBall()
 
-rightPaddle = Paddle(580,0,True)
-leftPaddle = Paddle(10,0,False)
+rightPaddle = Paddle(580,270,True)
+leftPaddle = Paddle(10,270,False)
+ball = Ball(300,300,10)
+
+
 fortsett = True
 while fortsett:
     for event in pg.event.get():
         if event.type == pg.QUIT:
             fortsett = False
     vindu.fill((255,255,255))
+
     trykkedeTaster = pg.key.get_pressed()
     rightPaddle.movePaddle(trykkedeTaster)
     leftPaddle.movePaddle(trykkedeTaster)
@@ -64,6 +88,8 @@ while fortsett:
 
     leftPoints = font.render(str(leftPaddle.points), True, (0,0,0))
     vindu.blit(leftPoints, (280,20))
+
+    ball.moveBall()
 
 
     pg.display.flip()
